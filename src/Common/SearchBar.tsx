9574c22b7d3bf6d625c6b7a38a56684ca84bb562
HeeSoon <phs970824@daum.net>
2023-06-09 18:32:04 +0900

    SearchBar size 변경, 검색 시 /favorite 페이지 이동

--- src/Common/SearchBar.tsx
+++ src/Common/SearchBar.tsx
@@ -1,45 +1,57 @@
 /** @jsxImportSource @emotion/react */
 import styled from '@emotion/styled';
+import { useContext, useState } from 'react';
 import {useNavigate} from 'react-router-dom';
+import {MyHero} from '../store/myhero';
+
+// onchange이벤트 re-render 발생으로 input의 아웃포커싱 이슈 처리를 위해 styled-component를 외부로 꺼냄 
+const Form = styled.form`
+  border-radius: 100px;
+  max-width: 350px;
+  display: flex;
+  justify-content: space-between;
+  background-color: white;
+  padding: 10px 20px 10px 10px;
+  @media screen and (max-width:768px) {
+    max-width: 300px;
+  }
+`
+const Input = styled.input`
+  min-width: 250px;
+  border: none;
+  padding: 5px;
+  outline: none;
+  @media screen and (max-width:768px) {
+    min-width: 200px;
+  }
+`
+const Btn = styled.button`
+  background-color: white;
+  border: none;
+  img{
+    width: 20px;
+  }
+`
 
 const SearchBar = () => {
-  const Form = styled.form`
-    border-radius: 100px;
-    max-width: 400px;
-    display: flex;
-    justify-content: space-between;
-    background-color: white;
-    padding: 10px 20px 10px 10px;
-    @media screen and (max-width:768px) {
-      max-width: 300px;
-    }
+  const [name, setName] = useState('');
+  const value = useContext(MyHero);
 
-  `
-  const Input = styled.input`
-    min-width: 300px;
-    border: none;
-    padding: 5px;
-    @media screen and (max-width:768px) {
-      min-width: 200px;
-    }
-  `
-  const Btn = styled.button`
-    background-color: white;
-    border: none;
-    img{
-      width: 20px;
-    }
-  `
-
-  let navigate = useNavigate();
-  function handleClick () {
-    navigate('/favorite/:name')
+  function handleGetName(e:React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
   }
+  
+  let navigate = useNavigate();
+  function handleFavoritePage (e:React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    value?.changeName(name);
+    navigate(`/favorite`);
+}
 
   return (
     <Form>
-      <Input type="text" placeholder='Search hero name!' name="search" />
-      <Btn type="submit" onClick={handleClick}><img src='/img/search.png' alt='검색'/></Btn>
+      <Input type="text" placeholder='Search hero name!' name="search" value={name} onChange={handleGetName}/>
+      <Btn type="submit" onClick={handleFavoritePage}><img src='/img/search.png' alt='검색'/></Btn>
     </Form>
   )
 
